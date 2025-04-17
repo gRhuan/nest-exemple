7c878b4ae058eeb1ced04de422ec5adb08323efe
Gladyson Rhuan <gladysonr04@gmail.com>
2025-04-17 19:47:57 -0300

    feat(repository): implement MembersRepository and PrismaMembersRepository for member management

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,25 +1,15 @@
-import { Controller, Post, Body } from '@nestjs/common';
-import { randomUUID } from 'node:crypto';
-import { PrismaService } from './database/prisma.service';
+import { Body, Controller, Post } from '@nestjs/common';
+import { MembersRepository } from './database/repositories/members.repository';
 import { CreateMemberBody } from './dto/createMember.dto';
 
 @Controller()
 export class AppController {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(private readonly membersRepository: MembersRepository) {}
 
   @Post()
   async create(@Body() body: CreateMemberBody) {
-    const { name, function: menberFunction } = body;
-    const member = await this.prisma.members.create({
-      data: {
-        id: randomUUID(),
-        name,
-        function: menberFunction,
-      },
-    });
+    const { name, function: memberFunction } = body;
 
-    return {
-      member,
-    };
+    await this.membersRepository.create(name, memberFunction);
   }
 }
